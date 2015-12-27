cdf4e91dc8d6ae058de6e48f6099992d392813a2
Yoshio Varney <yoshio.varney@gmail.com>
2015-12-26 21:49:16 -0800

    [feat] Finished autorating

--- plugin/popup.js
+++ plugin/popup.js
@@ -1,57 +1,29 @@
-$(document).ready(function() {
-    var toggleAuto = $("#toggleAuto");
-    var toggle = false;
+var $toggleAuto = $("#toggleAuto");
+var $ratings = $("input[name='rating']");
+var toggle;
 
+chrome.storage.local.get(["autorate", "rating"], function(settings) {
+  if (!settings["autorate"]) {
+    toggle = false;
+  } else {
+    toggle = true;
+    $toggleAuto.attr('checked', true);
+  }
 
-    document.addEventListener('DOMContentLoaded', function () {
-      toggleAuto.click(function() {
-        console.log('testing')
-      })
-          // document.querySelector('#toggleAuto').addEventListener('change', changeHandler);
-    });
+  var currentRating = settings["rating"] ? settings["rating"] : 5;
+  var checkedValue = "[value=" + currentRating + "]";
 
-    function changeHandler() {
-      toggle = !toggle;
-      console.log(toggle)
-    }
-
-    function init(){
-        attachEvents();
-        // getOptions();
-    }
-
-    function attachEvents(){
-        console.log('runniasdfng')
-        toggleAuto.on("click", setOptions);
-        toggleAuto.click(function() {
-            console.log('this is working')
-        })
-        chrome.runtime.onMessage.addListener(gotMessage);
-    }
-
-    function gotMessage(request, sender, sendResponse) {
-        if (sender.tab && request.closePopup){
-            window.close();
-        }
-    }
-
-    // function getOptions(){
-    //     chrome.storage.sync.get("qrEnable",gotOptions);
-    // }
-
-    // function gotOptions(data){
-    //     //console.log(data);
-    //     if(data.qrEnable == undefined){
-    //         chrome.storage.sync.set({"qrEnable":true});
-    //         data.qrEnable = true;
-    //     }
-    //     qrOption.prop("checked", data.qrEnable);
-    // }
-
-    function setOptions(){
-        console.log("setting changes");
-        // chrome.storage.sync.set({"qrEnable":qrOption.prop("checked")});
-    }
-
-    init();
+  if ($ratings.is(':checked') === false) {
+    $ratings.filter(checkedValue).prop('checked', true);
+  }
 });
+
+$toggleAuto.click(function() {
+  toggle = !toggle;
+  chrome.storage.local.set({"autorate": toggle});
+})
+
+$ratings.click(function() {
+  var rating = $ratings.filter(":checked").val();
+  chrome.storage.local.set({"rating": rating});
+})
--- plugin/rate.js
+++ plugin/rate.js
@@ -1,26 +1,55 @@
+var autorate, rating;
+var keycodes = {
+  49: 1,
+  50: 2,
+  51: 3,
+  52: 4,
+  53: 5
+};
+
 function rate(rating) {
     $('option[value="' + rating + '"]').attr('selected', 'selected').parent().focus();
     $('#ctl00_PageContent_submit').click();
+    console.log("Rating the song at a ", rating)
 }
 
 document.addEventListener('keydown', function(e) {
-  var keycode = e.keyCode;
-  switch(keycode) {
-    case 49:
-    console.log("rating 1")
-      rate(1);
-      break;
-    case 50: 
-      rate(2);
-      break;
-    case 51: 
-      rate(3)
-      break;
-    case 52: 
-      rate(4);
-      break;
-    case 53: 
-      rate(5);
-      break;
+  var keycode = keycodes[e.keyCode];
+  if (keycode) {
+    rate(keycode);
   }
 }, true);
+
+chrome.storage.local.get(["autorate", "rating"], function(settings) {
+  console.log("rating is: ", settings["rating"])
+  if (!settings["autorate"]) {
+    autorate = false;
+  } else {
+    autorate = true;
+  }
+
+  rating = settings["rating"]; 
+});
+
+chrome.storage.onChanged.addListener(function(changes, local) {
+  if (changes.autorate) {
+    if (changes.autorate.newValue) {
+      autorate = true;
+      console.log("Enabling autorating for DJCity Easy Rate");
+    } else {
+      autorate = false;
+      console.log("Disabling autorate for DJCity Auto Rate");
+    }
+  }
+
+  if (changes.rating) {
+    rating = changes.rating.newValue;
+    console.log("Songs will be rated at a ", rating);
+  }
+});
+
+$(function() {
+  if (autorate) {
+    rate(rating);
+  }
+})
